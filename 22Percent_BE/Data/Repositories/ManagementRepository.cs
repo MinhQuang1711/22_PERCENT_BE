19aec844792595cdf8b6183f6c7c960977a70e7d
MinhQuang1711 <phamminhquang.work@gmail.com>
2023-10-23 20:19:05 +0700

    Create/IngredientService

---
+++ 22Percent_BE/Data/Repositories/ManagementRepository.cs
@@ -0,0 +1,17 @@
+﻿using _22Percent_BE.Data.Repositories.IngredientRepo;
+using AutoMapper;
+
+namespace _22Percent_BE.Data.Repositories
+{
+    public class ManagementRepository : IManagementRepository
+    {
+        private readonly Lazy<IIngredientRepository> _lazyIngredientRepository;
+
+        public ManagementRepository(_22Context context,IMapper mapper) 
+        {
+            _lazyIngredientRepository = new Lazy<IIngredientRepository>(()=> new IngredientRepository(context,mapper));
+        }
+
+        public IIngredientRepository IngredientRepository => _lazyIngredientRepository.Value;
+    }
+}
---
+++ 22Percent_BE/Sevices/Ingredients/IIngredientService.cs
@@ -0,0 +1,15 @@
+﻿using _22Percent_BE.Data.DTOs.Ingredients;
+using _22Percent_BE.Data.Entities;
+
+namespace _22Percent_BE.Sevices.Ingredients
+{
+    public interface IIngredientService
+    {
+        public Task<bool> detete(string id);
+        public Task<List<Ingredient>> getAll();
+        public Task<Ingredient?> searchById(string id);
+        public Task<bool> create(CreateIngredientDto create);
+        public Task<string?> update(string id,UpdateIngredientDto update);
+        public Task<List<Ingredient>> searchByFilter(SearchIngredientDto search);
+    }
+}
