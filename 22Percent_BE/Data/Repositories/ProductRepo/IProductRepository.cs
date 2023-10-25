135965fb32c5fe870bad74e7722871402c7e1d2f
MinhQuang1711 <phamminhquang.work@gmail.com>
2023-10-25 10:47:25 +0700

    Create product repo

--- 22Percent_BE/Data/Repositories/ProductRepo/IProductRepository.cs
+++ 22Percent_BE/Data/Repositories/ProductRepo/IProductRepository.cs
@@ -1,9 +1,9 @@
-﻿//using _22Percent_BE.Data.DTOs.Products;
+﻿using _22Percent_BE.Data.DTOs.Products;
 
-//namespace _22Percent_BE.Data.Repositories.ProductRepo
-//{
-//    public interface IProductRepository
-//    {
-//        public Task<bool> create(CreateProductDto create);
-//    }
-//}
+namespace _22Percent_BE.Data.Repositories.ProductRepo
+{
+    public interface IProductRepository
+    {
+        public Task<bool> create(CreateProductDto create);
+    }
+}
